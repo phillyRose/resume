89bdfb62c59082cedb4f216096c6e7117e5adbdf
Thomas Hammons <thomas.hammons_jr@uconn.edu>
2016-02-06 16:14:13 -0500

    Create tom.php
    -tryna make a navbar

---
+++ tom.php
@@ -0,0 +1,102 @@
+<!-- TOM YOU HAVE TO CHANGE THE CSS DIRECTORY -->
+<?php
+	$i = 0;
+	function makeRoom( $howMuch )
+	{
+		while( $i < $howMuch )
+		{
+			echo "<br />";
+			$i++;
+		}
+	}
+?>
+<html>
+	<head>
+		<title> idk </title>
+		<link rel = "stylesheet" type = "text/css" href = "./resume.css" />
+		<script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"> </script>
+		<script language = "JavaScript">
+		<!--
+
+
+
+		//-->
+		</script>
+
+
+		
+		
+	</head>
+
+
+
+	<body>
+		<header id = "pageTop">
+			<h1>Thomas Hammons V - Resume </h1>
+		</header>
+		<div id = "pageWrapper">
+			<div id = "navbar">
+				<p id = "aboutMe"><a class = "navItem" href = "#">About Me</a></p>
+					<div id = "aboutMeDropped"></div>
+				<p><a class = "navItem" href = "#">Skill Summary</a></p>
+				<p><a class = "navItem" href = "#">Education Overview</a></p>
+				<p><a class = "navItem" href = "#">Interests/Hobbies</a></p>
+				<p><a class = "navItem" href = "#">Contact Me</a></p>
+			</div>
+
+			
+		
+
+
+
+
+		</div> <!-- End div pageWrapper -->
+
+
+
+
+
+		<?php
+			
+			
+
+
+
+		?>
+
+		<script language = "JavaScript">
+		<!--
+			var aboutMe = document.getElementById("aboutMe");
+			var newList = document.createElement("ul");
+			var newListItem = document.createElement("li");
+			var newListItemContent = document.createTextNode("Testing");
+			newListItem.appendChild( newListItemContent );
+			newList.appendChild( newListItem );
+			aboutMe.appendChild( newList );
+			var hideNewContent = document.getElementById("aboutMeDropped");
+			// hideNewContent.style.color = "blue";
+
+			
+
+
+
+		//-->
+		</script>
+
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
